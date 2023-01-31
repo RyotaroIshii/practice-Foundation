ab53aed986db02e200a569f79cd8b58a31e05ecf
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2023-01-31 19:45:56 +0000

    foundation practice

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "0d48985d451545af975e615bb79ea406.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
